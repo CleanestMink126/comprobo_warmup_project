2f8d8b145c5a8caefc9b37a5d15abbf8ebdbf299
CleanestMink126 <gns126@gmail.com>
2018-09-06 14:50:34 -0400

    added bump and accel

--- scripts/interface.py
+++ scripts/interface.py
@@ -2,6 +2,8 @@
 
 from geometry_msgs.msg import PointStamped
 from sensor_msgs import LaserScan
+from neato_node import Bump
+from neato_node import Accel
 import rospy
 
 class ReceiveMessageode(object):
@@ -23,6 +25,28 @@ class ReceiveLidar(object):
 
     def process_range(self, m):
         return m.range
+
+    def run(self):
+        rospy.spin()
+
+class ReceiveBump(object):
+    def __init__(self):
+        rospy.init_node('receive_lidar')
+        rospy.Subscriber("/bump", Bump, process_bump)
+
+    def process_bump(self, m):
+        return m
+
+    def run(self):
+        rospy.spin()
+
+class ReceiveAccel(object):
+    def __init__(self):
+        rospy.init_node('receive_lidar')
+        rospy.Subscriber("/accel", Accel, process_accel)
+
+    def process_accel(self, m):
+        return m
 
     def run(self):
         rospy.spin()
