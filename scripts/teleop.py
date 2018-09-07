8bb53d3e6eca1fbc031ee70d739ca01b39757583
sokuno222 <siena.okuno@students.olin.edu>
2018-09-07 14:01:28 -0400

    Object oriented teleop.py

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -2,16 +2,42 @@ import tty
 import select
 import sys
 import termios
+import interface
 
-def getKey():
-    tty.setraw(sys.stdin.fileno())
-    select.select([sys.stdin], [], [], 0)
-    key = sys.stdin.read(1)
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+if key == "a":
+    send_speed(0,0,0,0,0,1)
 
-settings = termios.tcgetattr(sys.stdin)
-key = None
-while key != '\x03':
-    key = getKey()
-    print key
+if key =="s":
+    send_speed(0,-1,0,0,0,0)
+
+if key == "w":
+    send_speed(0,1,0,0,0,0)
+
+if key == "s":
+    send_speed(0,0,0,0,0,-1)
+
+
+
+
+
+
+class TeleopC(object):
+    def __init__ (self):
+        self.settings = termios.tcgetattr(sys.stdin)
+
+    def getKey(self):
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
+
+    def run(self):
+        key=None
+        while key != '\x03':
+            key = self.getKey()
+            print key
+
+if __name__ == "__main__":
+    myTeleop = TeleopC()
+    myTeleop.run()
