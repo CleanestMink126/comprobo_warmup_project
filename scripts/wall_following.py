f2a7d005585303ed16732e297e24b2d26af18a91
sokuno222 <siena.okuno@students.olin.edu>
2018-09-11 11:15:01 -0400

    Added some if statements

--- scripts/wall_following.py
+++ scripts/wall_following.py
@@ -4,8 +4,25 @@
 values = np.array(m.ranges)
 no_zeros = values(np.where(values))
 
-#find minimum
+def run():
+    mytelC = TeleopC()
+    while mytelC.key != '\x03':
+        #find minimum
 
+        #check if surround values = d/cos(theta)
+        #returns (degree_index int, distance float) follows equation OR (None, None)
 
-#check if surround values = d/cos(theta)
-#returns None if there isnt one, or return a int if point follows equation
+        #if point returned, look at d
+        if ___ != (None,None):
+            if degree_index > 90:
+                if d <= 1.5:
+                    mytelC.myspeedctrl.send_speed(.75,.5)
+                elif d > 1.5:
+                    mytelC.myspeedctrl.send_speed(.75,-.5)
+            elif degree_index <90:
+                if d <= 1.5:
+                    mytelC.myspeedctrl.send_speed(.75,-.5)
+                elif d > 1.5:
+                    mytelC.myspeedctrl.send_speed(.75,.5)
+        else:
+            mytelC.myspeedctrl.send_speed(1,0)
